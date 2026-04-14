84e5b23a5a5e64845c0fd838906033522fa7f642
Priyansh Tyagi <tpriyansh973@gmail.com>
2026-04-14 15:22:02 +0530

    Backend Ready

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.use(
   }),
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 
 // 🔁 Rate Limiting – सिर्फ payment create-order पर ही लगाओ (verify पर नहीं)
 const paymentCreateLimiter = rateLimit({
